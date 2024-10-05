f2b7773f59d36f0810c2e7b2edf0752cb90daf27
VanQish <gymorin04@gmail.com>
2024-10-05 17:25:59 +0700

    Add files via upload

---
+++ bot/utils/banner.js
@@ -0,0 +1,14 @@
+function printBanner() {
+    console.log(`
+       ███╗   ██╗ ██████╗  ██████╗ ██████╗ 
+       ████╗  ██║██╔═══██╗██╔═══██╗██╔══██╗
+       ██╔██╗ ██║██║   ██║██║   ██║██████╔╝
+       ██║╚██╗██║██║   ██║██║   ██║██╔═══╝ 
+       ██║ ╚████║╚██████╔╝╚██████╔╝██║     
+       ╚═╝  ╚═══╝ ╚═════╝  ╚═════╝ ╚═╝     
+      `); 
+    console.log("© VanQish Bots");
+    console.log("t.me/AirdropSantuyy");
+  }
+  
+  module.exports = printBanner;
